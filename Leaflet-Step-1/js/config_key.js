2c8b2fb3b430dc313a2a1ef85e5264625caa0bde
josephsy96 <josephsy96@hotmail.com>
2020-02-20 19:11:15 -0600

    update key variable

---
+++ Leaflet-Step-1/js/config_key.js
@@ -0,0 +1,1 @@
+var API_KEY = "pk.eyJ1Ijoiam9zZXBoc3k5NiIsImEiOiJjajQzMHhmdjgwbmU1MzhxcXR2czA3czBnIn0.qMQqtPy_uPQs7mAnelldAw";
--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -2,13 +2,40 @@ let earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/
 
 //Perform API request for earthquake data
 d3.json(earthquake_url, function(data) {
-
+    earthquake_popup(data.features);
 });
 
 //Function to create popup data
 function earthquake_popup(e_data) {
 
+    function each_feature(feature,layer) {
+        layer.bindPopup("<h3>Place: " + feature.properties.place + "</h3><hr><p>Magnitude: " +
+        feature.properties.mag + "</p><br><p>Time: " + new Date(feature.properties.time) + "</p>");
+    }
 
+    //Creates layer with the appended earthquake data
+    let earth_geo = L.geoJSON(e_data, {
+        each_feature: each_feature
+    });
+
+    //Renders map with data
+    render_map(earth_geo);
 }
 
+//Function to create the map
 
+function render_map(earth) {
+
+    let light_map = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.light",
+        accessToken: API_KEY
+    });
+
+    let base = {
+        "Light Map": light_map
+    };
+
+    
+}
