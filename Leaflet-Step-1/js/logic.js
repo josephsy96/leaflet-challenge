a7aac799c7706562f657d13dd05c785fe3aad027
josephsy96 <josephsy96@hotmail.com>
2020-02-20 18:24:44 -0600

    starter code

---
+++ Leaflet-Step-1/js/logic.js
@@ -0,0 +1,14 @@
+let earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+//Perform API request for earthquake data
+d3.json(earthquake_url, function(data) {
+
+});
+
+//Function to create popup data
+function earthquake_popup(e_data) {
+
+
+}
+
+
