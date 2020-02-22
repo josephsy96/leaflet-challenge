d93d14c744ef780b360875729439fcc708c6b7b6
josephsy96 <josephsy96@hotmail.com>
2020-02-22 15:02:30 -0600

    magnitude circles

--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -10,15 +10,31 @@ d3.json(earthquake_url, function(data) {
 
 //Function to create popup data
 function earthquake_popup(e_data) {
+    console.log(e_data[0]);
 
-    function each_feature(feature,layer) {
-        layer.bindPopup("<h3>Place: " + feature.properties.place + "</h3><hr><p>Magnitude: " +
-        feature.properties.mag + "</p><hr><p>Time: " + new Date(feature.properties.time) + "</p>");
-        console.log(feature.properties.mag);
+    function magnitude_size(magnitude) {
+        return magnitude * 50000;
     }
 
-    //Creates layer with the appended earthquake data
+    function each_feature(feature,layer) {
+        // console.log(feature);
+        // console.log(layer.feature.properties);
+        layer.bindPopup("<h3>Place: " + feature.properties.place + "</h3><hr><p>Magnitude: " +
+        feature.properties.mag + "</p><hr><p>Time: " + new Date(feature.properties.time) + "</p>");
+        // console.log(feature.properties.mag);
+    }
+    
+    
+    // Creates layer with the appended earthquake data
     let earth_geo = L.geoJSON(e_data, {
+        pointToLayer: function(feature,latlng) {
+            return L.circle(latlng,{
+                    fillOpacity: 0.60,
+                    color: "white",
+                    fillColor: "cyan",
+                    radius: magnitude_size(feature.properties.mag)
+                });
+        },
         onEachFeature: each_feature
     });
 
@@ -39,10 +55,11 @@ function render_map(earth) {
 
     let sat_map = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 15,
+        maxZoom: 18,
         id: "mapbox.streets",
         accessToken: API_KEY
     });
+
 
     let boundaries = [
         [85.051129, -200], // East coordinates
@@ -69,4 +86,19 @@ function render_map(earth) {
     L.control.layers(base, overlay_map, {
         collapsed: true
     }).addTo(earth_map);
+
+
+    function legend_function(earth_data) {
+    //create legend for map
+    let info_legend = L.control({
+        position: "bottomright"
+    });
+
+    info_legend.onAdd = function() {
+        let div = L.DomUtil.create("div", "legend");
+        return div;
+    };
+
+    info_legend.addTo(earth_map);
+    }
 }
